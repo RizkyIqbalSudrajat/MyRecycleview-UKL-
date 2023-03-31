897d693202848374bae527be8c293b978179af61
RizkyIqbalSudrajat <rizkyiqbal2005@gmail.com>
2023-03-31 07:40:58 +0700

    first commit

---
+++ app/src/main/java/com/example/myrecycleview/HeroesData.kt
@@ -0,0 +1,75 @@
+package com.example.myrecycleview
+
+object HeroesData {
+    private val data = arrayOf(
+        arrayOf(
+            "Barcelona ",
+            "Didirikan pada tahun 1899 oleh sekelompok pemain Swiss, Inggris, Jerman dan Katalan yang dipimpin oleh Joan Gamper, klub telah menjadi simbol budaya Catalan dan Catalanisme, yang mempunyai motto \"Més que un club\" (Lebih dari sebuah klub)",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/4/47/FC_Barcelona_%28crest%29.svg/800px-FC_Barcelona_%28crest%29.svg.png"
+        ),
+        arrayOf(
+            "Real Madrid",
+            "Didirikan pada tahun 1902 sebagai Madrid Football Club, secara tradisional mengenakan kostum kandang putih. Kata Real (\"dari kerajaan\") Spanyol dan dianugerahkan ke klub oleh Raja Alfonso XIII pada tahun 1920 bersama-sama dengan mahkota kerajaan di lambang klub.",
+            "https://upload.wikimedia.org/wikipedia/id/8/8b/Real_Madrid_Club_de_F%C3%BAtbol.png"
+        ),
+        arrayOf(
+            "Manchester United",
+            " Didirikan sebagai Newton Heath LYR Football Club pada tahun 1878, klub ini berganti nama menjadi Manchester United pada 1902 dan pindah ke Old Trafford pada tahun 1910.\n" + "\n" + "Manchester United telah memenangkan banyak trofi di sepak bola Inggris, termasuk rekor 20 gelar Liga, 12 Piala FA,[3] 6 Piala Liga dan rekor 21 FA Community Shield. Klub ini juga telah memenangkan tiga Piala Eropa, satu Piala UEFA, satu Piala Winners UEFA, satu Piala Super UEFA, satu Piala Interkontinental dan satu Piala Dunia Antarklub FIFA. Pada 1998-1999, klub memenangkan treble dari Liga Premier, Piala FA dan Liga Champions, prestasi belum pernah terjadi sebelumnya untuk klub Inggris. Manchester United merupakan salah satu klub terbesar di Liga Utama Inggris sepanjang masa.",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/7/7a/Manchester_United_FC_crest.svg/800px-Manchester_United_FC_crest.svg.png"
+        ),
+        arrayOf(
+            "Manchester City",
+            "Manchester City Football Club (dikenal pula sebagai Man City atau The Citizens) adalah sebuah klub sepak bola profesional dari Inggris yang bermain di Liga Premier Inggris. Klub ini merupakan klub sekota dengan Manchester United dan bermarkas di Stadion Etihad, Manchester.",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/e/eb/Manchester_City_FC_badge.svg/800px-Manchester_City_FC_badge.svg.png"
+        ),
+        arrayOf(
+            "Paris Saint-Germain",
+            "Paris Saint-Germain (pengucapan bahasa Prancis: [paʁi sɛ̃ ʒɛʁmɛ̃]) atau dikenal luas dengan sebutan PSG merupakan sebuah tim sepak bola Prancis yang bermain di Ligue 1, Prancis. Bermarkas di Paris, Prancis. Klub ini didirikan pada 12 Agustus 1970, berkat penggabungan Paris FC dan Stade Saint-Germain. club yang sekarang telah menjelma menjadi salah satu club tarkam terbaik di eropa",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/e/e4/Paris_Saint-Germain_F.C..png/800px-Paris_Saint-Germain_F.C..png"
+        ),
+        arrayOf(
+            "Liverpool",
+            "sebuah klub sepak bola profesional asal Inggris yang berbasis di Liverpool. Didirikan pada tahun 1892, Liverpool kemudian bergabung dengan Football League di tahun berikutnya dan sejak pembentukannya memainkan pertandingan kandang mereka di Stadion Anfield yang terletak sekitar 4,8 km dari pusat kota.",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/0/0c/Liverpool_FC.svg/800px-Liverpool_FC.svg.png"
+        ),
+        arrayOf(
+            "Bayern Munchen",
+            "FC Bayern didirikan pada tahun 1900 oleh sebelas pemain sepak bola yang dipimpin oleh Franz John.[5] Meskipun Bayern memenangkan kejuaraan nasional pertamanya pada tahun 1932,[6] klub tersebut tidak dipilih untuk Bundesliga pada awal tahun 1963.[7] Klub memiliki periode yang sukses terbesar di tengah tahun 1970-an, di bawah kapten dari Franz Beckenbauer, memenangkan Piala Champions tiga kali berturut-turut (1974-1976). Secara keseluruhan, Bayern telah mencapai sebelas final Piala Champions/Liga Champions, yang terakhir memenangkan gelar keenam mereka pada tahun 2020 sebagai bagian dari treble benua. Bayern juga telah memenangkan satu Piala UEFA, satu Piala Winners UEFA, satu Piala Super UEFA, dua Piala Interkontinental dan satu Piala Dunia Antarklub FIFA, membuatnya menjadi salah satu klub Eropa yang paling sukses di dunia. Sejak pembentukan Bundesliga, Bayern telah menjadi klub yang dominan dalam sepak bola Jerman dengan 28 gelar dan telah memenangkan 10 dari 14 gelar terakhir. Mereka memiliki persaingan lokal tradisional dengan TSV 1860 München dan 1. FC Nürnberg, serta persaingan kontemporer dengan Borussia Dortmund.",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg/800px-FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg.png"
+        ),
+        arrayOf(
+            "Juventus",
+            "Juventus adalah klub tertua kedua di Italia,[12] setelah Genoa yang didirikan pada tahun 1893. Mereka tampil untuk pertama kalinya di liga divisi teratas Italia pada tahun 1900, setelah berganti nama menjadi Football Club Juventus. Mereka terus berada di kompetisi tertinggi tersebut (yang menggunakan nama Serie A sejak tahun 1929), kecuali pada musim 2006–2007.",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juventus_FC_2017_icon_%28black%29.svg/800px-Juventus_FC_2017_icon_%28black%29.svg.png"
+        ),
+        arrayOf(
+            "Chelsea ",
+            "Chelsea Football Club /ˈtʃɛlsiː/ adalah sebuah klub sepak bola yang bermarkas di Fulham, London. Didirikan pada 1905, klub ini kini berkompetisi di Liga Utama Inggris dan memainkan pertandingan kandang mereka di Stamford Bridge.[1][4] Chelsea merupakan salah satu klub sepak bola di Inggris tersukses, dengan telah memenangkan lebih dari tiga puluh gelar juara kompetisi klub domestik dan internasional.",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/c/cc/Chelsea_FC.svg/800px-Chelsea_FC.svg.png"
+        ),
+        arrayOf(
+            "Atletico Madrid",
+            "Atlético de Madrid nama lengkap Club Atletico de Madrid SAD (singkatan dari Sociedad Anónima Deportiva) (pengucapan bahasa Spanyol: [ˈkluβ atˈletiko ðe maˈðɾið]), atau Atlético Madrid, atau juga disebut Atlético atau Atleti, adalah tim sepak bola Spanyol yang bermain di La Liga Santander. Bermarkas di Madrid. Tim yang berdiri tahun 1903 memiliki banyak prestasi yang baik dipercaturan sepak bola Spanyol. Klub ini juga memiliki rivalitas dengan tetangganya Real Madrid, yang dikenal dengan Derbi Madrid.",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/f/f4/Atletico_Madrid_2017_logo.svg/800px-Atletico_Madrid_2017_logo.svg.png"
+        )
+    )
+
+
+    val listData: ArrayList<Hero>
+        get() {
+            val list = arrayListOf<Hero>()
+            for (aData in data) {
+                val hero = Hero()
+                hero.name = aData[0]
+                hero.from = aData[1]
+                hero.photo = aData[2]
+
+
+                list.add(hero)
+            }
+
+
+            return list
+        }
+}
+
